6a2582fe702f214912d3b30555eae7fa690dbc39
Yenifer <guajeyenifer@gmail.com>
2022-01-13 18:42:31 -0500

    Se finaliza la parte 2 del proyecto Shopping Cart

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import '../index.css'
 import { _getPhones } from '../utils/_DATA'
 import { receivePhones } from '../actions/phones'
 import Container from './container'
+import 'semantic-ui-css/semantic.min.css'
 
 
 class App extends Component {
