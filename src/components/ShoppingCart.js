0e85a364513b318c2475258a3c6cd4e76c7555b0
Yenifer <guajeyenifer@gmail.com>
2022-01-13 20:07:39 -0500

    Se finaliza la parte 4 del proyecto Shopping Cart

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react'
 import { DropTarget } from 'react-dnd'
-import Phone from './Phone'
 import { ItemTypes } from './Constants'
+import DisplayPhone from './DisplayPhone'
 
 // DnD Spec
 const ShoppingCartSpec = {
-    drop(){
-        return { name: 'ShoppingCart'}
+    drop() {
+        return { name: 'ShoppingCart' }
     }
 }
 
 // DnD DropTarget - collect
-let collect = ( connect, monitor )=>{
+let collect = (connect, monitor) => {
     return {
         connectDropTarget: connect.dropTarget(),
         isOver: monitor.isOver(),
@@ -20,26 +20,31 @@ let collect = ( connect, monitor )=>{
 }
 
 
-class ShoppingCart extends Component{
-    render(){
+class ShoppingCart extends Component {
+    render() {
+        const { inCart_phones } = this.props
         const { canDrop, isOver, connectDropTarget } = this.props;
-        const  isActive = canDrop && isOver;
+        const isActive = canDrop && isOver;
 
         let backgroundColor = '#FFFFFF';
-        if (isActive){
-            backgroundColor ='#F7F7BD';
-        } else if (canDrop){
+        if (isActive) {
+            backgroundColor = '#F7F7BD';
+        } else if (canDrop) {
             backgroundColor = '#F7F7F7';
         }
-        const style={
+        const style = {
             backgroundColor: backgroundColor
         };
         return connectDropTarget(
-            <div className="shopping-cart" style={ style } >
-                {
-                    isActive
-                    ? 'Humm, phone!'
-                    : 'Drag here to order!'
+            <div className="shopping-cart" style={style} >
+                {!inCart_phones.length &&
+                    (isActive
+                        ? 'Humm, phone!'
+                        : 'Drag here to order!')
+                }
+                {inCart_phones.length
+                    ? <DisplayPhone displayPhones={inCart_phones} />
+                    : null
                 }
             </div>
         )
